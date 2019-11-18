34166135b263138908d26ccdc0eb642b8e431e1d
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-11-18 12:52:33 +0200

    Lesson 5

---
+++ src/test/java/CreatePetTest.java
@@ -0,0 +1,45 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.Is.is;
+
+public class CreatePetTest {
+
+    @Test
+    public void createPet() {
+
+        String body = "{\n" +
+                "  \"id\": 0,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"kitty\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        RestAssured
+                .given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .post("https://petstore.swagger.io/v2/pet")
+                .then()
+                .statusCode(anyOf(is(202), is(201)))
+                .body("category.name", is(not("")))
+                .log().all();
+    }
+
+}
